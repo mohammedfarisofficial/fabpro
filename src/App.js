36fab5e45fc4d70f3760c1612afc3f746f211869
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2021-12-03 01:32:57 +0530

    to show the review

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './styles/globalStyle.scss';
-import Navbar from './componets/Navbar/Navbar';
-import Home from './componets/pages/home/Home';
+import { useRef, useEffect } from "react";
+import "./styles/globalStyle.scss";
+import "./styles/locomotive.scss";
+import Navbar from "./componets/Navbar/Navbar";
+import Home from "./componets/pages/home/Home";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
 function App() {
+  const appRef = useRef(null);
+
   return (
-    <>
-      <Navbar/>
-      <Home/>
-    </>
+    <LocomotiveScrollProvider options={{ smooth: true }} containerRef={appRef}>
+      <div data-scroll-container ref={appRef}>
+        <Navbar />
+        <Home />
+      </div>
+    </LocomotiveScrollProvider>
   );
 }
 
--- src/componets/pages/home/Home.js
+++ src/componets/pages/home/Home.js
@@ -2,31 +2,34 @@ import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./home.scss";
+//locomotive
+import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 const Home = () => {
   const pathRef = useRef(null);
   const overlayRef = useRef(null);
-  const [overlay, setOverlay] = useState(false);
+  const [ hidden,setHidden] = useState(false)
+
+
   //gsap animation
   const tl = gsap.timeline({ paused: true });
-
+  
   const first = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
   const second = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
   const third = "M 0 0 V 0 Q 50 0 100 0 V 0 z";
 
   const animate = () => {
-    tl.to(pathRef.current, 0.8, { attr: { d: first }, ease: "Power2.easeIn" })
+      tl.to(pathRef.current, 0.8, { attr: { d: first }, ease: "Power2.easeIn" })
       .to(pathRef.current, 0.8, { attr: { d: second }, ease: "Power2.easeOut" })
       .to(pathRef.current, 0.8, {
         attr: { d: third },
         ease: "Power2.easeOut",
         onComplete: () => {
-          overlayRef.current.classList.add("hide-overlay");
-          // overlayRef.current.classList.remove("menu-wrap--open");
+          setHidden(true)
         },
       })
-
-      // tl.to(pathRef.current, 0.8, { attr: { d: forth }, ease: 'Power2.easeOut' })
       .play(0);
   };
 
@@ -36,17 +39,20 @@ const Home = () => {
     trigger: ".box",
     start: "top top",
     once: true,
-    onEnter: () => console.log("enter"),
+    onEnter: () => {
+      animate()
+    }
   });
+  const scroll = new LocomotiveScroll();
 
   return (
-    <div className="home-wrapper">
+    <div className="home-wrapper" data-scroll-section >
       <div className="home-landing"></div>
       <div  className="home-gallery">
-        <div className="box" onClick={animate}></div>
+        <div data-scroll data-scroll-speed="3" data-scroll-position="top" className="box" onClick={animate}></div>
         <svg
           ref={overlayRef}
-          className="overlay"
+          className={ hidden ? 'hide-overlay overlay' : 'overlay'}
           viewBox="0 0 100 100"
           preserveAspectRatio="none"
           fill="#ff0000"
@@ -63,6 +69,9 @@ const Home = () => {
             <mpath xlinkHref="#path" />
           </animateMotion>
         </svg>
+        <div className="home-gallery-flex">
+
+        </div>
       </div>
     </div>
   );
